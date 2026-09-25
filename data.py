4b72b6e3955a0b28bb6678532321a9108a603255
prestonb25 <prestonb25@nycstudents.net>
2026-09-25 06:20:35 -0700

    Update data.py

--- data.py
+++ data.py
@@ -4,7 +4,15 @@ def tip_calculator (c):
 bill = 50.75
 s= input ("How was your service") 
 print (s)
-g= input("good")
-print(g)
-o= input("okay")
+1= input("bad")
+2= input("okay")
+3= input("good")
+4= ("great")
+choice= input ("Enter the numner (1-4):")
 
+if choice== "1":
+        tip_percent= 0.00
+
+elif choice== "2":
+   tip_percent= 0.10
+   
